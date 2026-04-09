5508fc8e18349cc1e17b3e59318070dcdfec6262
roshan-sk <roshansk03@gmail.com>
2026-04-09 18:58:50 +0530

    Update path configuration and updated uploaded files folder

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory('input', filename)
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 
 if __name__ == "__main__":
